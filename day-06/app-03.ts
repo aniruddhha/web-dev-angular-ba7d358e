8ae6a8b99a9c22bd8e9ab75d484e80a849fb7457
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-12-28 19:53:58 +0530

    es6: code modification

--- day-06/app-03.ts
+++ day-06/app-03.ts
@@ -1,9 +1,10 @@
-var nm = 'hi'
+var nm = 'h'
+var pq = '2020-01-01'
 
-console.log('Name is ' + nm) // concat prior to es6
+console.log('Name is ' + nm + pq) // concat prior to es6
 
 // ES6 -> ECMAScript 6 -> 2015
-console.log(`Name is ${nm}`) 
+console.log(`Name is ${nm} ${pq}`) 
 console.log(`Square of 2 ${2*2}`) 
 
 
