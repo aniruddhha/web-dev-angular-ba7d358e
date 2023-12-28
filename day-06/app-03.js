deabbfecd3b6e6c958dc5404105db6d29bd2569d
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-12-28 19:45:49 +0530

    es6: string interpolation

---
+++ day-06/app-03.js
@@ -0,0 +1,5 @@
+var nm = 'hi';
+console.log('Name is ' + nm); // concat
+// ES6 -> ECMAScript 6 -> 2015
+console.log("Name is ".concat(nm));
+console.log("Square of 2 ".concat(2 * 2));
---
+++ day-06/app-03.ts
@@ -0,0 +1,9 @@
+var nm = 'hi'
+
+console.log('Name is ' + nm) // concat prior to es6
+
+// ES6 -> ECMAScript 6 -> 2015
+console.log(`Name is ${nm}`) 
+console.log(`Square of 2 ${2*2}`) 
+
+
