7d924336bb419621339cac5269c77d7aa96ef090
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-12-29 19:50:09 +0530

    typescript: query solved

--- day-07/app-01.ts
+++ day-07/app-01.ts
@@ -1,12 +1,15 @@
 
-sayHello()
-
-function sayHello() {
+function sayHello() { //  function defination
     console.log('Hello')
 }
+
+sayHello() // function call
 
 var fn = function() {
     console.log(`This is anonomyous function`)
 }
 
 fn()
+
+var fn1 = sayHello // refering to sayHello, not call
+fn1()
