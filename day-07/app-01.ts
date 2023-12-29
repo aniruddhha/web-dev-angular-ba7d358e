78000c01acbabb15d3359025680c49ff56816d3a
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-12-29 19:42:48 +0530

    typescript: function basics

---
+++ day-07/app-01.ts
@@ -0,0 +1,12 @@
+
+sayHello()
+
+function sayHello() {
+    console.log('Hello')
+}
+
+var fn = function() {
+    console.log(`This is anonomyous function`)
+}
+
+fn()
---
+++ day-07/app-02.ts
@@ -0,0 +1,10 @@
+
+function normalFun() {
+
+}
+
+var fn1: () => void = function() {
+
+}
+
+var fn2: () => void = normalFun
