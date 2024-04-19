ef799777d67f03c6f941d73b76a4d549a7fee53d
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-04-19 19:14:59 +0530

    css3: postions

--- day-36/🧑‍💻app.js
+++ day-36/🧑‍💻app.js
@@ -1,4 +1,16 @@
-const a = {}
-const b = {}
+const ob1 = {a : 1} // ['a']
+const ob2 = {b : 1} // ['b']
 
-console.log(a === b) // false
+console.log(ob1 == ob2) // false
+const cnd1 = Object.keys(ob1).length == Object.keys(ob2).length
+const cnd2 = Object.keys(ob1).every(key => ob1[key] === ob2[key] )
+
+const arr = Object.keys(ob1)
+for( let i = 0; i < arr.length; i++ ) {
+    const key = arr[i]
+    ob1[key] == ob2[key]
+}
+
+console.log(ob1['a'])
+
+console.log('1' == 1)
